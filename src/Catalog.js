6dba86318754976f58bcd7dea1dd8f894fa0f36d
Maham Ghazanfar <66554243+MahamG99@users.noreply.github.com>
2021-04-09 14:39:45 +0500

    initial commit

--- src/Catalog.js
+++ src/Catalog.js
@@ -1,4 +1,5 @@
 import "./styles.css";
+import "./maham.css";
 import CustomerNavbar from "./CustomerNavbar";
 import Memory from "./Memory";
 import BottomBar from "./BottomBar";
@@ -36,38 +37,7 @@ const Catalog = () => {
         artist: "Wasif",
         price: "400",
         image: "https://enjoycss.com/bg-img/custom/20013-j3vxrl.njd7f.jpg"
-      },
-      {
-        title: "Green Khussa",
-        artist: "Wasif",
-        price: "400",
-        image: "https://enjoycss.com/bg-img/custom/20013-j3vxrl.njd7f.jpg"
-      },
-      {
-        title: "Green Khussa",
-        artist: "Wasif",
-        price: "400",
-        image: "https://enjoycss.com/bg-img/custom/20013-j3vxrl.njd7f.jpg"
-      },
-      {
-        title: "Green Khussa",
-        artist: "Wasif",
-        price: "400",
-        image: "https://enjoycss.com/bg-img/custom/20013-j3vxrl.njd7f.jpg"
-      },
-      {
-        title: "Green Khussa",
-        artist: "Wasif",
-        price: "400",
-        image: "https://enjoycss.com/bg-img/custom/20013-j3vxrl.njd7f.jpg"
-      },
-      {
-        title: "Green Khussa",
-        artist: "Wasif",
-        price: "400",
-        image: "https://enjoycss.com/bg-img/custom/20013-j3vxrl.njd7f.jpg"
-      },
-      
+      }
     ]
   };
 
@@ -75,20 +45,12 @@ const Catalog = () => {
     return state.products.map((product, index) => {
       const { title, artist, price, image } = product; //destructuring
       return (
-        <table class="box">
-          <tr>
-            <th class="table">
-              <img src={image} alt="Italian Trulli" id="image" />
-            </th>
-            <th class="productDetails">
-              <p class="title">
-                <a href="catalog.html">{title}</a>
-              </p>
-              <p class="artist">Artist: {artist}</p>
-              <p class="price">PKR {price}</p>
-            </th>
-          </tr>
-        </table>
+        <div className="product-div">
+          <img className="product-img" src={image} alt="product" />
+          <h3>{title}</h3>
+          <h5>Artist: {artist}</h5>
+          <h5>Price: Rs {price}</h5>
+        </div>
       );
     });
   };
@@ -100,12 +62,29 @@ const Catalog = () => {
       <h1>Catalog</h1>
       <br></br>
       <div class="filters">
-        <input type="button" class="sortbars" value="Sort A-Z" />
-        <input type="button" class="sortbars" value="Sort $-$$" />
-        <input type="button" class="sortbars" value="Filter" />
+        <input type="button" className="sortbars" value="Sort A-Z" />
+        <input type="button" className="sortbars" value="Sort $-$$" />
+        <input type="button" className="sortbars" value="Filter" />
       </div>
-      <br></br>
-      <div class="itemboxes">{renderProducts()}</div>
+      <div className="main">
+        <div className="input-group">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search Products"
+          ></input>
+          <div className="input-group-append">
+            <button className="btn btn-secondary" type="button">
+              <i className="fa fa-search"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+      <br />
+      <div className="space"></div>
+      <div className="catalog-adjust">{renderProducts()}</div>
+      {/* <div class="itemboxes">{renderProducts()}</div> */}
+      <BottomBar />
     </div>
   );
 };
